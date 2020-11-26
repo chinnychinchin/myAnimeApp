93b9ea75a3029f7448e2e4bca4c3385227bbfdfc
chinyanlim <chinyan77@gmail.com>
2020-11-26 17:49:26 +0800

    added share function

--- src/app/components/results.component.ts
+++ src/app/components/results.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import {HttpParams} from "@angular/common/http";
+import { NgNavigatorShareService } from 'ng-navigator-share';
 
 @Component({
   selector: 'app-results',
@@ -10,16 +11,18 @@ import {HttpParams} from "@angular/common/http";
 })
 export class ResultsComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient) { }
+  constructor(private activatedRoute: ActivatedRoute, private http: HttpClient, private webshare: NgNavigatorShareService) { }
 
   type
   q: string
-  results
+  results //[ {}, {}, {}]
+  canShare = false
 
   ngOnInit(): void {
+    this.canShare = this.webshare.canShare()
     this.type = this.activatedRoute.snapshot.params.type;
     this.q = this.activatedRoute.snapshot.params.q;
-    this.apiRequest().then(result => { this.results = result['results']})
+    this.apiRequest().then(result => { this.results = result['results']; console.log(this.results)})
   }
 
 
@@ -29,4 +32,16 @@ export class ResultsComponent implements OnInit {
     return this.http.get(`https://api.jikan.moe/v3/search/${this.type}`, {params: params}).toPromise()
   }
 
+  shareThis(id) {
+
+    console.log(id) //21
+    const r = this.results.find(v =>  v.mal_id == id) //r = { }
+    console.log('>>> r =', r)
+    this.webshare.share({
+      title: r['title']
+    }).catch(e => {console.log(e)} )
+  }
+
 }
+
+
